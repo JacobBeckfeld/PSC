d9be1ad9a5b63b07d692411f9ef943d35898b444
JacobBeckfeld <jacbeck456@gmail.com>
2021-09-02 13:30:56 -0500

    Added basic server file

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const mongoose = require("mongoose");
+
+const config = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/d3", config);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => console.log(` Now listening on localhost:${PORT}`));
+});
